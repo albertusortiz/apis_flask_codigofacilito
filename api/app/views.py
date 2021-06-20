647fc90a6906bdddf5ad0b8c425dd8a3d497310e
Alberto Ortiz <alberto.ortiz.vargas@gmail.com>
2021-06-19 20:19:17 -0500

    Construyendo endpoint para mostrar una tarea mediante el ID.

--- api/app/views.py
+++ api/app/views.py
@@ -7,7 +7,6 @@ api_v1 = Blueprint('api', __name__, url_prefix='/api/v1')
 
 @api_v1.route('/tasks', methods=['GET'])
 def get_tasks():
-
     tasks = Task.query.all() # SELECT * FROM tasks;
 
     return response([
@@ -15,8 +14,10 @@ def get_tasks():
     ])
 
 @api_v1.route('/tasks/<id>', methods=['GET'])
-def get_task():
-    pass
+def get_task(id):
+    task = Task.query.filter_by(id=id).first()
+
+    return response(task.serialize())
 
 @api_v1.route('/tasks/', methods=['POST'])
 def create_task():
